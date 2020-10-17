668a20d26ff0dc39694538a26f77d0a3604bfe1c
Chinyere Ordor <chinyereordor@gmail.com>
2020-10-17 15:04:04 +0100

    PHP in 15

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+
+    <title>Document</title>
+</head>
+<body>
+    <!-- <?php
+        // $loggedIn = true;
+
+        // if ($loggedIn == true){
+        //     echo " you are in";
+        // } else {
+        //     echo "Log in";
+        // }
+    ?> -->
+    <form action="process.php" method="post">
+        Enter your name <input name="name" type="text">
+        <input type="submit">
+    </form>
+
+    
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Document</title>
+</head>
+<body>
+
+<!-- <?php
+    $name = $_POST["name"];
+    echo "Hello, " .$name;
+?> -->
+
+<?php
+    
+?>
+    
+</body>
+</html>
